74cf58879c2ae767da6fa62ec81eb4d56b7e79ec
Douglas Ricardo Domiciano de Campos <douglasdrdc@gmail.com>
2017-07-21 16:23:38 -0300

    inclusao da conexao mongo db

---
+++ Blog.Infra.Data.Mongo/Data/PostagemData.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Infra.Data.Mongo.Data
+{
+    public class PostagemData
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+        public string Titulo { get; set; }
+        public string Subtitulo { get; set; }
+        public string Texto { get; set; }
+    }
+}
---
+++ Blog.Infra.Data.Mongo/Util/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Infra.Data.Mongo.Util
+{
+    public class Util
+    {
+        public static readonly string MongoClientConnection = @"mongodb://localhost/";
+        public static readonly string MongoDataBase = "dbBlog";
+
+
+    }
+}
